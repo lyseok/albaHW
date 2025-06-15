a775a2950ffc55459e16ddb5c71ed2f4f34983fd
hongtaegyu <hongtaegyu@218.38.137.27>
2025-06-15 12:50:50 +0900

    수

--- src/main/java/kr/or/ddit/alba/service/AlbaService.java
+++ src/main/java/kr/or/ddit/alba/service/AlbaService.java
@@ -3,7 +3,7 @@ package kr.or.ddit.alba.service;
 import java.util.List;
 
 public interface AlbaService {
-	public void createAlba( alba);
+	public void createAlba(AlbaVO alba);
 	public List<AlbaVO> readAlbaList();
 	public List<AlbaVO> readDetailAlbaList(AlbaVO alba);
 	public AlbaVO readAlba(String id);
