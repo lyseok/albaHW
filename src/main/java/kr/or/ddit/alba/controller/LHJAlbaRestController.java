3ede0026e5a3001207e4577a76a771d2762b2381
notbinary-hj <sleepingc0ya@gmail.com>
2025-06-16 01:35:56 +0900

    licenseImage 를 albaId에 따라 조회 가능한 메서드 추가

--- src/main/java/kr/or/ddit/alba/controller/LHJAlbaRestController.java
+++ src/main/java/kr/or/ddit/alba/controller/LHJAlbaRestController.java
@@ -5,69 +5,68 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.ddit.alba.service.AlbaService;
+import kr.or.ddit.licalba.service.LicAlbaService;
 import kr.or.ddit.vo.AlbaVO;
-import kr.or.ddit.vo.LicenseVO;
+import kr.or.ddit.vo.LicAlbaVO;
 import lombok.RequiredArgsConstructor;
 
-//@RestController
-@CrossOrigin(origins = "http://localhost:5173")  //security 안 쓰고, 필터 지정 없을 시 어노테이션 통해 등록 필수.
-//@RequestMapping({"/rest/alba", "/ajax/alba"})
+/**
+ * REST API를 이용한 Alba & License 관리
+ * React 클라이언트와 JSON 통신
+ * base URL: /rest/alba
+ */
+
+@RestController  //@ResponseBody는 얘랑 중복됨
+@CrossOrigin(origins = "http://localhost:5173") // security 안 쓰고, 필터 지정 없을 시 어노테이션 통해 등록 필수.
+@RequestMapping("/rest/alba")
 @RequiredArgsConstructor
 public class LHJAlbaRestController {
-	private final AlbaService service;
+	private final AlbaService albaService;
+	private final LicAlbaService licAlbaService;
 
-   @GetMapping
-   @ResponseBody
-   public List<AlbaVO> getAlbaList() {
-      return service.readAlbaList();
-   }
-   
-   @GetMapping("/{alba}")
-   public AlbaVO getAlba(@PathVariable String alba) {  //경로의존변수 받아서 거기로 이동시 이거 타게
-      return service.readAlba(alba);
-   }
+	@GetMapping
+	public List<AlbaVO> getAlbaList() {
+		return albaService.readAlbaList();
+	}
 
-   // 등록 - PRG 패턴
-   @PostMapping
-   public Map<String, Object> createAlba(@RequestBody  AlbaVO alba) {  //body 있으므로 어노테이션 필수
-      service.createAlba(alba);
-      return Map.of("ok", true);
-   }
-   
-   // 수정
-   @PutMapping("/{albaId}")
-   public Map<String, Object> editAlba(
-      @PathVariable String albaId
-      , @RequestBody AlbaVO alba
-   ) {
-      alba.setAlId(albaId);
-      service.modifyAlba(alba);
-      return Map.of("ok", true);   //json 응답으로 { "ok": true } 나감
-   }
-   
-   @DeleteMapping("/{albaId}")
-   public Map<String, Object> deleteAlba(@PathVariable String albaId) {
-      service.removeAlba(albaId);
-      return Map.of("ok", true);
-   }
-   
-   @GetMapping("/{albaId}/licenseImage")
-   public List<LicenseVO> getAlbalicense(@PathVariable String albaId) {
-      return service.readLicenseList();
-   }
+	@GetMapping("/{alba}")
+	public AlbaVO getAlba(@PathVariable String alba) { // 경로의존변수 받아서 거기로 이동시 이거 타게
+		return albaService.readAlba(alba);
+	}
+
+	// 등록 - PRG 패턴
+	@PostMapping
+	public Map<String, Object> createAlba(@RequestBody AlbaVO alba) { // body 있으므로 어노테이션 필수
+		albaService.createAlba(alba);
+		return Map.of("ok", true);
+	}
+
+	// 수정
+	@PutMapping("/{albaId}")
+	public Map<String, Object> editAlba(@PathVariable String albaId, @RequestBody AlbaVO alba) {
+		alba.setAlId(albaId);
+		albaService.modifyAlba(alba);
+		return Map.of("ok", true); // json 응답으로 { "ok": true } 나감
+	}
+
+	@DeleteMapping("/{albaId}")
+	public Map<String, Object> deleteAlba(@PathVariable String albaId) {
+		albaService.removeAlba(albaId);
+		return Map.of("ok", true);
+	}
+
+	@GetMapping("/{albaId}/licenseImage")
+	public List<LicAlbaVO> getAlbalicense(@PathVariable String albaId) {
+		return licAlbaService.readLicAlbaList(albaId);
+	}
 
 }
